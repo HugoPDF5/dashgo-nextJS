6ae328c0b6824ee2484288e500d900c84e9f5909
Hugo <hugopatricio51@gmail.com>
2022-08-28 15:55:02 -0300

    feat: adding icons in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 const Header = () => {
   return (
@@ -47,6 +47,25 @@ const Header = () => {
     />
 
     <Icon as={RiSearchLine} fontSize='20' />
+    </Flex>
+
+    <Flex
+      align='center'
+      ml='auto'
+    > 
+    <HStack spacing='8' mx='8' pr='8' py='1' color='gray.300' borderRightWidth={1} borderColor='gray.700' >
+      <Icon as={RiNotificationLine} fontSize='20' /> 
+      <Icon as={RiUserAddLine} fontSize='20' /> 
+    </HStack>
+
+    <Flex align='center'> 
+      <Box mr='4' textAlign='right'>
+        <Text>Hugo</Text>
+        <Text color='gray.300'>hugo@gmail.com</Text>
+      </Box>
+
+      <Avatar size='md' name='Hugo Patricio'/>
+    </Flex>
     </Flex>
 
     </Flex>
